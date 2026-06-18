6e4d18352de3032da22d442ae691578a732ebc35
Dante Hdz <dnte18@gmail.com>
2026-06-17 21:33:21 -0600

    feat: agregar validaciones Data Annotations al modelo Producto

--- EcoSystem.Data/Models/Producto.cs
+++ EcoSystem.Data/Models/Producto.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EcoSystem.Data.Models
 {
     public class Producto
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(120)]
         public string Nombre { get; set; } = string.Empty;
+
+        [Required]
         public decimal Precio { get; set; }
+
         public int Stock { get; set; }
     }
 }
