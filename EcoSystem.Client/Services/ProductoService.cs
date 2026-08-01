532aba8abc8d876387c3e570c537c716ceaaabe5
Dante Hdz <dnte18@gmail.com>
2026-07-31 20:30:53 -0600

    feat: implementar peticion HTTP GET en ProductoService

--- EcoSystem.Client/Services/ProductoService.cs
+++ EcoSystem.Client/Services/ProductoService.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -31,6 +33,39 @@ namespace EcoSystem.Client.Services
             var response = await _httpClient.PostAsJsonAsync("api/Productos", nuevoProducto);
 
             return response.IsSuccessStatusCode;
+        }
+
+        // --- NUEVA FIRMA 6: MÉTODO GET ---
+        public async Task<List<Producto>> GetProductosAsync()
+        {
+            try
+            {
+                string token = await _tokenService.GetTokenAsync();
+
+                if (!string.IsNullOrEmpty(token))
+                {
+                    _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                }
+
+                // Hacemos el GET al endpoint
+                var response = await _httpClient.GetAsync("api/Productos");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // Deserializamos el JSON de tu API a una lista en C#
+                    var productos = await response.Content.ReadFromJsonAsync<List<Producto>>();
+                    return productos ?? new List<Producto>();
+                }
+                else
+                {
+                    throw new Exception($"Error al obtener productos: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error en GetProductosAsync: {ex.Message}");
+                throw;
+            }
         }
     }
 }
